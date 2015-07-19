9bffde2df8e91bf63d28ce7ad9777b7ef10bed27
Diego Guerra <dgoguerra.or@gmail.com>
2015-07-20 00:23:30 +0200

    finished demo

--- demo/DemoPaginatorApp.jsx
+++ demo/DemoPaginatorApp.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var Paginator = require('../Paginator.jsx');
 
+var sprintf = require("sprintf-js").sprintf;
+
 var App = React.createClass({
     getInitialState: function() {
         return { currPage: 2, lastPage: 10 };
@@ -9,31 +11,49 @@ var App = React.createClass({
         this.setState({ currPage: currPage });
     },
     changeCurrPageEvent: function(evt) {
-        this.changeCurrPage(evt.target.value);
+        var currPage = Number(evt.target.value);
+        this.setState({
+            currPage: Math.min(currPage, this.state.lastPage)
+        });
     },
     onChangePagesNumEvent: function(evt) {
-        var numPages = evt.target.value;
+        var numPages = Number(evt.target.value);
         this.setState({
-            currPage: Math.max(this.state.currPage, numPages),
+            currPage: Math.min(numPages, this.state.currPage),
             lastPage: numPages
         });
     },
     render: function() {
-        console.log(this.state);
+        var code = sprintf(
+            "<Paginator currPage={%s} lastPage={%s} onChange={/* function */} />",
+            this.state.currPage,
+            this.state.lastPage
+        );
+
         return (
-            <div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <h1>Demo</h1>
+                </div>
                 <div className="col-sm-6 col-md-4">
                     <div className="form-group">
                         <label>Current page:</label>
-                        <input type="text" className="form-control" value={this.state.currPage} onChange={this.changeCurrPageEvent} />
+                        <input type="text" className="form-control" value={this.state.currPage}
+                            onChange={this.changeCurrPageEvent} />
                     </div>
                     <div className="form-group">
                         <label>Number of pages:</label>
-                        <input type="text" className="form-control" value={this.state.lastPage} onChange={this.onChangePagesNumEvent} />
+                        <input type="text" className="form-control" value={this.state.lastPage}
+                            onChange={this.onChangePagesNumEvent} />
                     </div>
                 </div>
+                <div className="col-sm-12 col-md-8">
+                    <label>The component arguments are: </label>
+                    <pre><code>{code}</code></pre>
+                </div>
                 <div className="col-xs-12">
-                    <Paginator currPage={this.state.currPage} lastPage={this.state.lastPage} onChange={this.changeCurrPage} />
+                    <Paginator currPage={this.state.currPage} lastPage={this.state.lastPage}
+                        onChange={this.changeCurrPage} />
                 </div>
             </div>
         );
